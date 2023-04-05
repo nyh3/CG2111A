e3cf74cf2bd65db3c0e3302f26eb928a17b62ace
nyh3 <110619373+nyh3@users.noreply.github.com>
2023-04-05 16:30:34 +0800

    Add files via upload

--- Alex2_test/Ultrasonicsensor.ino
+++ Alex2_test/Ultrasonicsensor.ino
@@ -1,25 +1,53 @@
 
 // Ultrasonic sensor pins
-#define TRIGPIN A0
-#define ECHOPIN A1
+#define TRIGPINF A0
+#define ECHOPINF A1
+#define TRIGPINB A2
+#define ECHOPINB A3
 
 void ultrasetup() {
-  pinMode(TRIGPIN, OUTPUT); // Sets the TRIGPIN as an Output
-  pinMode(ECHOPIN, INPUT); // Sets the ECHOPIN as an Input
+  // Sets the TRIGPIN as an Output
+  pinMode(TRIGPINF, OUTPUT); 
+  pinMode(TRIGPINB, OUTPUT); 
+  // Sets the ECHOPIN as an Input
+  pinMode(ECHOPINF, INPUT); 
+  pinMode(ECHOPINB, INPUT); 
+  
 }
 
-bool too_close() {
+bool too_close_forward() {
   long duration;
   long distance;
   // Clears the TRIGPIN
-  digitalWrite(TRIGPIN, LOW);
+  digitalWrite(TRIGPINF, LOW);
   delayMicroseconds(2);
   // Sets the TRIGPIN on HIGH state for 10 microseconds
-  digitalWrite(TRIGPIN, HIGH);
+  digitalWrite(TRIGPINF, HIGH);
   delayMicroseconds(10);
-  digitalWrite(TRIGPIN, LOW);
-  // Reads the ECHOPIN, returns the soundwave travel yime in microseconds
-  duration = pulseIn(ECHOPIN, HIGH);
+  digitalWrite(TRIGPINF, LOW);
+  // Reads the ECHOPIN, returns the soundwave travel time in microseconds
+  duration = pulseIn(ECHOPINF, HIGH);
+  // Calculating the distance
+  distance = (duration * 0.034) / 2;
+  if (distance <= 5) {
+    return true;
+  } else {
+    return false;;
+  }
+}
+
+bool too_close_backwards() {
+  long duration;
+  long distance;
+  // Clears the TRIGPIN
+  digitalWrite(TRIGPINB, LOW);
+  delayMicroseconds(2);
+  // Sets the TRIGPIN on HIGH state for 10 microseconds
+  digitalWrite(TRIGPINB, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(TRIGPINB, LOW);
+  // Reads the ECHOPIN, returns the soundwave travel time in microseconds
+  duration = pulseIn(ECHOPINB, HIGH);
   // Calculating the distance
   distance = (duration * 0.034) / 2;
   if (distance <= 5) {
